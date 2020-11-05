5af17868d7d1781e4e9492ca81856a3d2d1f280a
fwg-dev <fwgicheh@gmail.com>
2020-11-05 15:41:03 -0800

    set up redux store with dependecies

---
+++ src/store.js
@@ -0,0 +1,26 @@
+import {
+  createStore, 
+  applyMiddleware,
+  combineReducers, 
+  compose
+} from 'redux';
+import thunk from 'redux-thunk';
+
+import trips from './reducers/trips';
+import tripFormData from './reducers/tripFormData'; 
+
+const composeEnhancer = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+
+const reducers = combineReducers ({
+  trips, 
+  tripFormData
+
+});
+
+const store = createStore(
+  reducers,
+  composeEnhancer(applyMiddleware(thunk))
+);
+
+export default store
