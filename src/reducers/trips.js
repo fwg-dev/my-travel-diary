9d40a755409eeea34e464c298eccf72b45fa2bb2
fwg-dev <fwgicheh@gmail.com>
2020-11-05 15:40:34 -0800

    add tripForm data and trips reducers

---
+++ src/reducers/trips.js
@@ -0,0 +1,13 @@
+export default (state =[], action) => {
+
+  switch(action.type) {
+    case 'GET_TRIPS_SUCCESS':
+      return action.trips; 
+
+      case 'CREATE_TRIP_SUCCESS':
+      return state.concat(action.trip);
+
+      default: 
+      return state; 
+  }
+}
