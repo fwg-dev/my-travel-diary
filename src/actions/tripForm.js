645ea74d1bd01a6719df0f4f4e955790d3e685c4
fwg-dev <fwgicheh@gmail.com>
2020-11-06 13:40:25 -0800

    modify form action

--- src/actions/tripForm.js
+++ src/actions/tripForm.js
@@ -1,5 +1,5 @@
 //const { default: tripFormData } = require("../reducers/tripFormData");
-
+//Action Creator 
 export const updateTripFormData = tripFormData => {
   return {
     type: 'UPDATED_DATA',
