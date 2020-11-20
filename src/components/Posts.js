a10efae2acb945f155b845bad0d2b79887cf1f2f
fwg-dev <fwgicheh@gmail.com>
2020-11-19 21:26:58 -0800

    added div

---
+++ src/components/Posts.js
@@ -0,0 +1,17 @@
+// list of all posts associated with one Trip! 
+
+import React from 'react'; 
+
+
+const Posts = (props) => {
+
+  return (
+    <dv>
+   
+    </dv>
+    
+  )
+
+}
+
+export default Posts; 
