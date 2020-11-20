0beae86d9a5e5a27b88bab977c4f17d8f06ff701
fwg-dev <fwgicheh@gmail.com>
2020-11-19 21:20:48 -0800

    add routes

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,49 +1,41 @@
-import React, { Component } from 'react'; 
-import Trips from './Trips';
+import React from 'react'; 
 // import Navbar from './components/Navbar'
-import './App.css';
+import Nav from '../components/Navbar';
+import Home from '../pages/Home';
+import Profile from '../pages/Profile'; 
+import PersonalTrips from '../pages/PersonalTrips'
+import TripDetail from '../components/TripDetail'
+
+//Router 
+import { Route, Switch } from 'react-router-dom'; 
+
 
 
 class App extends React.Component { 
-
-  // componentDidMount() {
-  //   fetch('http://localhost:3001/api/vi/users',{
-  //     method: 'GET'
-  //   })
-  //   .then(res => res.json())
-  //   .then(data => console.log(data))
-
-
-  // }
-      
-
-
-
-
-
-
-
+     
   render() {
     return (
 
-      
+      <div> 
+          <Nav />
+          <Switch >
+          <Route path="/" exact>
+             <Home /> 
+          </Route>
+          <Route path="/profile"  exact>
+            <Profile />
+          </Route>
+          <Route path="/trips"  exact>
+            <PersonalTrips/>
+          </Route> 
+          <Route path="/trips/:id" exact>
 
-      <div className="App"> 
-        <div className="App-header">
-          <div className="topnav">
-            <p>THE VAULT </p>
-          </div>
-          
-          <div className="topnav-right">
-            <p>PROFILE</p>
-            <p>LOG OUT</p>
-          </div>
-          
-        </div>
+             <TripDetail />
+          </Route>
+         
 
-
-      
-        <Trips />
+          </Switch>
+         
       </div>
       
       
