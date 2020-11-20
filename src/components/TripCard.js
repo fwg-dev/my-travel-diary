d2be5687ba1b58d397d4cd962d36afb6437847df
fwg-dev <fwgicheh@gmail.com>
2020-11-19 21:20:14 -0800

    add link to trip card

--- src/components/TripCard.js
+++ src/components/TripCard.js
@@ -1,15 +1,24 @@
 import React from 'react'; 
+import {Route, Link} from 'react-router-dom'; 
+
 
 
 const TripCard = ({ trip }) => (
   <div className="TripDetails">
      <div key={trip.id} className="TripCard">
+      <div>
         <img className= "TripImage" src={trip.img_url} alt={trip.title} />
-        <h3>{trip.title}</h3>
+      </div>
+      <Link to={`/trips/${trip.id}`}> {trip.title}</Link>
         <h5>{trip.city}, {trip.country}</h5>
         <h5>{trip.date_of_trip} </h5>
         {/* <h5>{trip.user_id} </h5> */}
-     </div>
+        <div>
+          {/* <button name='delete'className="Trash"> Delete</button> */}
+          {/* <button name='edit'className="Edit">Edit</button> */}
+       </div>
+      </div>
   </div>
 )
-export default TripCard; 
+
+export default TripCard;
