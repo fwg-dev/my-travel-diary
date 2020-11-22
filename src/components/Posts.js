e32330004f3979eba75455666bb2710b7e7587a6
fwg-dev <fwgicheh@gmail.com>
2020-11-22 02:18:20 -0800

    passed the post props to the function

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,14 +1,25 @@
-// list of all posts associated with one Trip! 
+// list of all posts associated with one trip! 
 
 import React from 'react'; 
 
 
 const Posts = (props) => {
-
+  // console.log(props.posts)
+  // debugger; 
   return (
-    <dv>
-   
-    </dv>
+    <div>
+    {props.posts && props.posts.map(post =>             
+     <div key={post.id}>
+        <h5>{post.heading}</h5> 
+        <p>{post.description}</p>
+        <div>
+        <img className= "PostImage" src={post.img} alt={post.heading} />
+      </div>
+   </div>
+        
+    
+    )}
+    </div>
     
   )
 
