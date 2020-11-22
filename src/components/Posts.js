8fe95daf0942e707fd5b89ea877e84dbb043990a
fwg-dev <fwgicheh@gmail.com>
2020-11-22 04:31:08 -0800

    add delete functionality

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,20 +1,32 @@
 // list of all posts associated with one trip! 
-
 import React from 'react'; 
+import {connect} from 'react-redux'; 
 
+import { deletePost} from '../actions/posts'; 
 
 const Posts = (props) => {
-  // console.log(props.posts)
-  // debugger; 
+
+  const handleDelete =(post)=>{
+    props.deletePost(post.id, post.trip_id)   
+
+  }
+
+
   return (
     <div>
     {props.posts && props.posts.map(post =>             
      <div key={post.id}>
-        <h5>{post.heading}</h5> 
-        <p>{post.description}</p>
+         <h5>{post.heading}</h5> 
+         <p>{post.description}</p>
         <div>
-        <img className= "PostImage" src={post.img} alt={post.heading} />
-      </div>
+         <img className= "PostImage" src={post.img} alt={post.heading} />
+        </div>
+      <button 
+      onClick={ () => handleDelete(post)}
+
+      
+      
+      >Delete</button>
    </div>
         
     
@@ -25,4 +37,4 @@ const Posts = (props) => {
 
 }
 
-export default Posts; 
+export default  connect(null, {deletePost})(Posts); 
