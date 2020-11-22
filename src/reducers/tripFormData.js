9d37a56d8e4af3d6b985ba6b477860b117d75d9c
fwg-dev <fwgicheh@gmail.com>
2020-11-22 02:05:13 -0800

    rm trip form data from redux store

--- src/reducers/tripFormData.js
+++ src/reducers/tripFormData.js
@@ -1,22 +1,22 @@
-const initialState = {
-  title: '',
-  city: '',
-  country: '',
-  date_of_trip: 0, 
-  // user_id: ''
+// const initialState = {
+//   title: '',
+//   city: '',
+//   country: '',
+//   date_of_trip: 0, 
+//   // user_id: ''
 
-}
+// }
 
-export default (state = initialState, action) => {
+// export default (state = initialState, action) => {
 
-switch(action.type) {
-  case 'UPDATED_DATA':
-    return action.tripFormData;
+// switch(action.type) {
+//   case 'UPDATED_DATA':
+//     return action.tripFormData;
 
-    case 'RESET_TRIP_FORM':
-      return initialState;
+//     case 'RESET_TRIP_FORM':
+//       return initialState;
 
-    default: 
-    return state; 
-}
-}
+//     default: 
+//     return state; 
+// }
+// }
