b6960b933029937aed4f8ff38a7e3ba4871869fc
fwg-dev <fwgicheh@gmail.com>
2020-11-22 02:01:44 -0800

    add post reducer

--- src/reducers/trips.js
+++ src/reducers/trips.js
@@ -1,11 +1,21 @@
-export default (state = [], action) => {
 
+export default (state = [], action) => {
+debugger; 
   switch(action.type) {
     case 'GET_TRIPS_SUCCESS':
       return action.trips; 
 
       case 'CREATE_TRIP_SUCCESS':
       return state.concat(action.trip);
+      case 'ADD_POST':
+        let trips = state.trips.map(trip =>{
+          if (trip.id === action.payload.id) {
+             return action.payload
+          }else {
+            return trip
+          }
+        })
+        return {...state, trips}
 
       default: 
       return state; 
